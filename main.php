3f6f12ab2b47c53dabb7a939ec744df286636dd4
DoomGuy1818 <zasdqs745@gmail.com>
2024-06-20 14:49:40 +0300

    add interfaces

--- main.php
+++ main.php
@@ -1,1 +1,67 @@
 <?php
+        class News {
+           private string $id;
+           private string $title;
+           private string $description;
+
+            /**
+             * @var Source[]
+             */
+           private array $sourses = [];
+        }
+
+        class Source {
+            private string $id;
+            /**
+             * @var Publisher[]
+             */
+            private array $publishers = [];
+            private string $link;
+            private string $type; //XML HTML JSON
+            private string $summary;
+        }
+
+        class Publisher{
+            private string $id;
+            private string $name;
+            private string $logo;
+
+
+        }
+
+
+
+        interface ParserInterface {
+            public function key(): string;
+            public function parseNews(string $xml):array;
+        }
+
+
+        interface DataFormInterface {   //Промежуточный слой
+            public function NewsTransform(array $news):array;
+        }
+
+        interface DataSaveInterface {
+            public function NewsSave(array $news):bool;
+        }
+
+        class NullParser implements ParserInterface {
+            public function __construct(
+                private readonly Source $source
+            ){}
+
+            public function key(): string{
+                return $this->source->getName();
+            }
+
+            public function parseNews(string $xml): array
+            {
+                return [];
+            }
+        }
+
+        class newsDTO {
+            private string $id;
+            private string $title;
+            private string $description;
+        }
